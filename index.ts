be5b60cb31f502a964132e423a3a6c9cb13eda5d
Elias Landin <elias.landin@me.com>
2025-09-29 15:43:20 +0200

    Rename emails to recipient addresses

--- index.ts
+++ index.ts
@@ -1,10 +1,14 @@
 import { readFileSync } from "node:fs";
+import path from "path";
 
-const PATH_TO_EMAILS = process.env.PATH_TO_EMAILS!;
+const PATH_TO_RECIPIENT_ADDRESSES = process.env.PATH_TO_RECIPIENT_ADDRESSES!;
 
 let emails: string[] = [];
 try {
-  const csv = readFileSync(PATH_TO_EMAILS, "utf8");
+  const csv = readFileSync(
+    path.join(process.cwd(), PATH_TO_RECIPIENT_ADDRESSES),
+    "utf8",
+  );
   emails = csv.split("\n").filter((email) => email !== "");
 } catch (e) {
   console.error(e);
