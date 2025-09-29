ee729f0d5e891ddacc0f8d164f65b5b5acabaca5
Elias Landin <elias.landin@me.com>
2025-09-29 15:53:01 +0200

    Small fix

--- index.ts
+++ index.ts
@@ -3,13 +3,13 @@ import path from "path";
 
 const PATH_TO_RECIPIENT_ADDRESSES = process.env.PATH_TO_RECIPIENT_ADDRESSES!;
 
-let emails: string[] = [];
+let recipients: string[] = [];
 try {
   const csv = readFileSync(
     path.join(process.cwd(), PATH_TO_RECIPIENT_ADDRESSES),
     "utf8",
   );
-  emails = csv.split("\n").filter((email) => email !== "");
+  recipients = csv.split("\n").filter((email) => email !== "");
 } catch (e) {
   console.error(e);
 }
