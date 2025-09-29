4a8db1032e5c3c8ca544986dd51eee0e9e87a8a2
Elias Landin <elias.landin@me.com>
2025-09-29 15:56:50 +0200

    Read email HTML from file

--- index.ts
+++ index.ts
@@ -2,14 +2,18 @@ import { readFileSync } from "node:fs";
 import path from "path";
 
 const PATH_TO_RECIPIENT_ADDRESSES = process.env.PATH_TO_RECIPIENT_ADDRESSES!;
+const PATH_TO_EMAIL_HTML = process.env.PATH_TO_EMAIL_HTML!;
 
 let recipients: string[] = [];
+let emailHtml: string;
 try {
   const csv = readFileSync(
     path.join(process.cwd(), PATH_TO_RECIPIENT_ADDRESSES),
     "utf8",
   );
   recipients = csv.split("\n").filter((email) => email !== "");
+
+  emailHtml = readFileSync(PATH_TO_EMAIL_HTML, "utf8");
 } catch (e) {
   console.error(e);
 }
